c7b3f12c5144f937f9a2154c85edd0722d026816
Stephen Richard <smrTucson@gmail.com>
2023-12-14 19:25:02 -0700

    working on Quarto; try to execute and get output.

--- .github/actions/github_action_main.py
+++ .github/actions/github_action_main.py
@@ -59,10 +59,9 @@ def main():
         sys.exit(-1)
 
 def _quarto_render_html(markdown_in:str, output_path:str):
-    with open(output_path, "w") as f:
-        print("In githubActionMain: Quarto render: ",markdown_in,  output_path)
-        result = subprocess.run(["/opt/quarto/bin/quarto", "render", markdown_in, "--to html"], stdout=f)
-        print("Quarto call result ", result.returncode)
+     print("In githubActionMain: Quarto render: ",markdown_in,  output_path)
+     result = subprocess.run(["/opt/quarto/bin/quarto", "render", markdown_in, "--to html"], stdout=f)
+     print("Quarto call result ", result.returncode)
 #    resultfile = open(output_path, "r")
 #    print("output path content: ", resultfile.read())
 #    resultfile.close()
