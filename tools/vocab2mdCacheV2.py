f2a34c206514eadbd4bbb9d284b6bc0a1349031f
Stephen Richard <smrTucson@gmail.com>
2023-12-18 12:12:55 +0800

    clean out docs

--- tools/vocab2mdCacheV2.py
+++ tools/vocab2mdCacheV2.py
@@ -244,6 +244,8 @@ def describeNarrowerTerms(g, v, r, depth=0, level=[]):
 def describeVocabulary(G, V):
     res = []
     level = [1, ]
+    print(f"vocab2md: {G} graph input")
+
     # this is the header for Quarto in the markdown output
     res.append("---")
     res.append("comment: | \n  WARNING: This file is generated. Any edits will be lost!")
@@ -266,7 +268,7 @@ def describeVocabulary(G, V):
     if len(gobj)>0:
         scheme = gobj[0]
     else:
-#        print(f"vocab2md: {V} object must have a skos:prefLabel")
+        print(f"vocab2md: {V} object must have a skos:prefLabel")
         return 1
 #    scheme = getObjects(G, V, skosT("prefLabel"))[0]
     lscheme = scheme.replace(" ","")
@@ -353,7 +355,7 @@ def main(source, vocabulary):
     res = []
 
     vocabulary = store.expand_name(vocabulary)
-#    print(f"vocabulary name: {vocabulary}")
+    print(f"vocabulary name: {vocabulary}")
     theMarkdown = describeVocabulary(store._g, vocabulary)
     res.append(theMarkdown)
 
